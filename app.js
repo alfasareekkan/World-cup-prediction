6d52dd830fcdd891bffa73d8135c2685cb20a5bc
MOHAMMED ALFAS A <87485303+alfasareekkan@users.noreply.github.com>
2022-12-12 14:49:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from 'cookie-parser'
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const app = express();
+app.use(express.static('public'));
+app.use(express.json())
+
+
+// view engine
+app.set('view engine', 'hbs');
+mongoose.connect(process.env.DATABASE).then(() => {
+  app.listen(process.env.PORT, () =>
+    console.log(`server running on port : ${process.env.PORT}`)
+  );
+});
+
+
+app.use("/", (req, res) => {
+    res.render('home')
+})
