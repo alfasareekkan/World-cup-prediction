16a0b9c4fe867701286844684f9248caedb9c8f1
chmubarak <chmohamedmubarak@gmail.com>
2022-12-13 19:25:09 +0530

    add 404

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
-import cookieParser from 'cookie-parser'
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import userRouter from './routes/userRouter.js'
+import userRouter from "./routes/userRouter.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
-app.use(express.static('public'));
-app.use(express.json())
-app.use(express.urlencoded())
-
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded());
 
 // view engine
-app.set('view engine', 'hbs');
-mongoose.connect(process.env.DATABASE).then(() => {
-  app.listen(process.env.PORT, () =>
-    console.log(`server running on port : ${process.env.PORT}`)
-  );
-});
+app.set("view engine", "hbs");
 
-
-app.use(userRouter)
+// mongoose.connect(process.env.DATABASE).then(() => {
+  app.listen(8000, () => console.log(`server running on port : ${8000}`));
+  // });
+  app.use(userRouter);
+  
+  app.use(function (req, res, next) {
+    res.status(404);
+    if (req.accepts("html")) {
+      res.render("404");
+      return;
+    }
+  });
